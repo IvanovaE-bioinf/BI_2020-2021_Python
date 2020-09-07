5f70072c394bcf1a3d4c68dbfd905e2b2e71ba7c
StrebulaevaEA <ivanovae.bioinf@gmail.com>
2020-09-07 20:05:21 +0300

    File calculator.py was added

---
+++ calculator.py
@@ -0,0 +1,11 @@
+a, b, c = int(input()), str(input()), int(input())
+if b == "+":
+    print(a + c)
+elif b == "-":
+    print(a - c)
+elif b == "/":
+    print(a / c)
+elif b == "*":
+    print(a * c)
+else:
+    print("Mission impossible!")
