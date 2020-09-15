0d030fccb7e0e9e8b03e4c467c0fbfd60f5c56a9
StrebulaevaEA <ivanovae.bioinf@gmail.com>
2020-09-15 21:23:46 +0300

    File calculator.py was changed

--- calculator.py
+++ calculator.py
@@ -1,11 +1,20 @@
-a, b, c = int(input()), str(input()), int(input())
-if b == "+":
-    print(a + c)
-elif b == "-":
-    print(a - c)
-elif b == "/":
-    print(a / c)
-elif b == "*":
-    print(a * c)
+number_1 = float(input("Enter a number: "))
+operator = input("Enter an operator: ")
+number_2 = float(input("Enter a number: "))
+if operator == "+":
+    print(number_1 + number_2)
+elif operator == "-":
+    print(number_1 - number_2)
+elif operator == "/":
+    if number_2 == 0:
+        print("Invalid operation - division by zero!")
+    else:
+        print(number_1 / number_2)
+elif operator == "*":
+    print(number_1 * number_2)
+elif operator == "//":
+    print(number_1 // number_2)
+elif operator == "%":
+    print(number_1 % number_2)
 else:
-    print("Mission impossible!")
+    print("Mission impossible - invalid operator!")
