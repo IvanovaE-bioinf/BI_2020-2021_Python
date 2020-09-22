3affcc0a5e3aca3bad88f6793195fbd325341927
StrebulaevaEA <ivanovae.bioinf@gmail.com>
2020-09-22 22:16:14 +0300

    File converter.py was changed

--- converter.py
+++ converter.py
@@ -1,82 +1,42 @@
 print("Конвертер величин (массы, площади, температуры).")
-my_num = float(input("Введите значение: "))
-magnitude = int(input("Введите физическую величину (масса - 1, площадь - 2, температура - 3): "))
+print("Введите значение и единицы измерения (кг, г, мг, мкг, ц, т; м^2, км^2, га, а, дм^2, см^2, мм^2; Ц, К, Ф):")
+my_num, my_num_units = input().split()
+my_num = float(my_num)
+d1 = {'кг': 1, 'г': 1000, 'мг': 1000000, 'мкг': 1000000000, 'ц': 0.01, 'т': 0.001}  # коэффициенты пересчета
+d2 = {'м^2': 1, 'км^2': 0.000001, 'га': 0.0001, 'а': 0.01, 'дм^2': 100, 'см^2': 10000, 'мм^2': 1000000}
+d3 = ['Ц', 'К', 'Ф']
 
-if magnitude == 1:
+if my_num_units in d1:
+    SI = my_num / d1[my_num_units]  # перевод в единицу массы в системе СИ
+    print(my_num, my_num_units, "- это:")
+    for key in d1:  # перебираем ключи, пропуская исходные единицы измерения
+        if key != my_num_units:
+            print(d1[key] * SI, key)
+        else:
+            continue
 
-    d = {'кг': 1, 'г': 1000, 'мг': 1000000, 'мкг': 1000000000, 'ц': 0.01, 'т': 0.001}  # коэффициенты пересчета
-    my_num_units = input("Введите единицы измерения (кг, г, мг, мкг, ц, т): ")
+elif my_num_units in d2:
+    SI = my_num / d2[my_num_units]
+    print(my_num, my_num_units, "- это:")
+    for key in d2:  # перебираем ключи, пропуская исходные единицы измерения
+        if key != my_num_units:
+            print(d2[key] * SI, key)
+        else:
+            continue
 
-    if my_num_units not in d:  # проверка вводимых единиц измерения
-        print("Неподдерживаемые единицы измерения!")
-    else:
-        SI = my_num / d[my_num_units]
-        '''if my_num_units == "кг":
-            
-            SI = my_num  # потому что кг - это единица массы в системе СИ
-        elif my_num_units == "г":
-            SI = my_num * 0.001  # если другие, то пересчитываем сначала в единицы СИ
-        elif my_num_units == "мг":
-            SI = my_num * 0.000001
-        elif my_num_units == "мкг":
-            SI = my_num * 0.000000001
-        elif my_num_units == "ц":
-            SI = my_num * 100
-        elif my_num_units == "т":
-            SI = my_num * 1000'''
+elif my_num_units in d3:  # пересчет по формулам
+    if my_num_units == "Ц":
         print(my_num, my_num_units, "- это:")
-        for key in d:  # перебираем ключи, пропуская исходные единицы измерения
-            if key != my_num_units:
-                print(d[key] * SI, key)
-            else:
-                continue
-
-elif magnitude == 2:
-
-    d = {'м^2': 1, 'км^2': 0.000001, 'га': 0.0001, 'а': 0.01, 'дм^2': 100, 'см^2': 10000, 'мм^2': 1000000}
-    my_num_units = input("Введите единицы измерения (км^2, м^2, га, а, дм^2, см^2, мм^2): ")
-
-    if my_num_units not in ["км^2", "м^2", "га", "а", "дм^2", "см^2", "мм^2"]:
-        print("Неподдерживаемые единицы измерения!")
-    else:
-        if my_num_units == "м^2":
-            SI = my_num
-        if my_num_units == "км^2":
-            SI = my_num * 1000000
-        elif my_num_units == "га":
-            SI = my_num * 10000
-        elif my_num_units == "а":
-            SI = my_num * 100
-        elif my_num_units == "дм^2":
-            SI = my_num * 0.01
-        elif my_num_units == "см^2":
-            SI = my_num * 0.0001
-        elif my_num_units == "мм^2":
-            SI = my_num * 0.000001
+        print(my_num + 273.15, "К")
+        print(my_num * (9 / 5) + 32, "Ф")
+    elif my_num_units == "К":
         print(my_num, my_num_units, "- это:")
-        for key in d:
-            if key != my_num_units:
-                print(d[key] * SI, key)
-            else:
-                continue
-
-elif magnitude == 3:
-
-    my_num_units = input("Введите единицы измерения (град_Ц, град_К, град_Ф): ")
-
-    if my_num_units == "град_Ц":  # пересчет по формулам
+        print(my_num - 273.15, "Ц")
+        print((my_num - 273.15) * (9 / 5) + 32, "Ф")
+    elif my_num_units == "Ф":
         print(my_num, my_num_units, "- это:")
-        print(my_num + 273.15, "град_К")
-        print(my_num * (9 / 5) + 32, "град_Ф")
-    elif my_num_units == "град_К":
-        print(my_num, my_num_units, "- это:")
-        print(my_num - 273.15, "град_Ц")
-        print((my_num - 273.15) * (9 / 5) + 32, "град_Ф")
-    elif my_num_units == "град_Ф":
-        print(my_num, my_num_units, "- это:")
-        print((my_num - 32) * (5 / 9), "град_Ц")
-        print((my_num - 32) * (5 / 9) + 273.15, "град_К")
-    else:
-        print("Неподдерживаемые единицы измерения!")
+        print((my_num - 32) * (5 / 9), "Ц")
+        print((my_num - 32) * (5 / 9) + 273.15, "К")
+
 else:
-    print("Неподдерживаемая физическая величина!")
+    print("Неподдерживаемые единицы измерения!")
