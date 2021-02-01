d9489153770d1621deffa9cc7467b35faba6a32e
IvanovaE <ivanovae.bioinf@gmail.com>
2021-02-01 15:25:38 +0300

    Folder Homework_painting with files was created

---
+++ Homework_painting/Length_distribution.py
@@ -0,0 +1,28 @@
+import matplotlib.pyplot as plt
+plt.rcParams['savefig.bbox'] = 'tight'
+plt.figure(figsize=(10, 8))
+path = input('Enter path to fastq file: ')
+
+
+def length_distribution(x):
+    with open(x, 'r') as f:
+        lines = f.readlines()
+        lens = []
+        for i in range(1, len(lines), 4):
+            lens.append(len(lines[i].rstrip()))
+        plt.hist(lens, bins=20, color='green')
+        plt.title('Sequence length distribution')
+        plt.xlabel('Sequence length')
+        plt.ylabel('Frequency')
+        plt.grid()
+        plt.savefig('Sequence_length.png')
+        plt.show()
+
+
+length_distribution(path)
+
+
+
+
+
+
---
+++ Homework_painting/Simple_line_plot.py
@@ -0,0 +1,28 @@
+import matplotlib.pyplot as plt
+plt.rcParams['savefig.bbox'] = 'tight'
+plt.figure(figsize=(10, 8))
+y = [i for i in range(1, 20)]
+xs = [[i for i in range(1, 20)]]
+for i in range(1, 4):
+    xs.append([j + 2 for j in xs[i - 1]])
+graph_number = 0
+lstyle = ['-', '--', '-.', ':']
+for i in range(len(xs)):
+    graph_number += 1
+    plt.plot(xs[i], y, label=str(graph_number), linestyle=lstyle[i])
+plt.title('Line plots')
+plt.xlabel('$x$')
+plt.ylabel('$y$')
+plt.grid()
+plt.legend()
+plt.savefig('Line_plots.png')
+plt.show()
+
+
+
+
+
+
+
+
+
