34e26fee875b353850e984bc3ffd187a12f9e2d0
IvanovaE <ivanovae.bioinf@gmail.com>
2021-10-27 22:27:37 +0300

    Code was improved

--- Homework_pandas/Pandas_test.py
+++ Homework_pandas/Pandas_test.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 # Plotting of the stacked barplot
 plt.rcParams['savefig.bbox'] = 'tight'
 train = pd.read_csv('train.csv')
-pos = list(train.iloc[:, list(train.columns).index('pos')])
+pos = train['pos'].to_list()
 nucleotides = train.filter(items=['A', 'T', 'G', 'C'])
 nucleotides.index = pos
 ax = nucleotides.plot(kind='bar', stacked=True, figsize=(10, 6))
@@ -18,6 +18,10 @@ plt.show()
 # Creating train_part.csv
 matches_mean = train.loc[:, 'matches'].mean()
 train_matches = train.query('matches > @matches_mean')
-train_part = train_matches.filter(items=['pos', 'reads_all', 'mismatches', 'deletions', 'insertions'])
+train_part = train_matches.filter(items=['pos',
+                                         'reads_all',
+                                         'mismatches',
+                                         'deletions',
+                                         'insertions'])
 train_part.to_csv("train_part.csv", index=False)
 
